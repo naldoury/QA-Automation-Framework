44829db1a3296d213cb1ba53208a0142eab8c02a
Nagam <naldoury@gmail.com>
2022-10-23 23:30:25 -0700

    added Homework15

---
+++ src/test/java/Homework15.java
@@ -0,0 +1,58 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Homework15 {
+
+    @Test
+    public static void LoginvalidEmailvalidPasswordTest ()throws InterruptedException
+    {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        String url = "https://bbb.testpro.io/";
+        driver.get(url);
+        Assert.assertEquals(driver.getCurrentUrl(), url);
+
+
+
+        WebElement emailField = driver.findElement(new By.ByCssSelector("[type='email']"));
+        emailField.click();
+        emailField.sendKeys("demo@class.com");
+
+
+        WebElement passwordField = driver.findElement(new By.ByCssSelector("[type='password']"));
+        passwordField.click();
+        passwordField.sendKeys("te$t$tudent");
+
+        WebElement submitButton = driver.findElement(new By.ByCssSelector("[type='submit']"));
+        submitButton.submit();
+        WebElement avatarIcon = driver.findElement( By.className("avatar"));
+        Assert.assertTrue(avatarIcon.isDisplayed());
+
+        WebElement songsList = driver.findElement( By.className("songs"));
+        Assert.assertTrue(songsList.isDisplayed());
+        songsList.click();
+
+
+        WebElement songsTitle = driver.findElement( By.className("title"));
+        String expectedSongName = "pluto";
+        String songName = driver.findElement( By.className("title")).getText();
+        if(expectedSongName==songName)
+            System.out.println("Yes, pluto song is on the list");
+        else
+            System.out.println("pluto song is not found!");
+
+
+        Thread.sleep(3000);
+        driver.quit();
+    }
+
+
+}
